b47b4c1e2ab38f8b5480903e34444bb13cec9fce
Olalla <olalla.lopeznavarro-casas@iesvalleinclan.es>
2025-04-25 12:38:06 +0200

    si

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-from fastapi import FastAPI
+# main.py
 
-# from routers import tecnico, cliente, productos, incidencias, tiempos, factura, ubicacion
+from fastapi import FastAPI
+from routers import tecnico, cliente, productos, incidencias, tiempos, factura, ubicacion
 
 app = FastAPI()
 
+# Incluir todos los routers
+app.include_router(tecnico.router)
+app.include_router(cliente.router)
+app.include_router(productos.router)
+app.include_router(incidencias.router)
+app.include_router(tiempos.router)
+app.include_router(factura.router)
+app.include_router(ubicacion.router)
 
-# app.include_router(tecnico.router)
-# app.include_router(cliente.router)
-# app.include_router(productos.router)
-# app.include_router(incidencias.router)
-# app.include_router(tiempos.router)
-# app.include_router(factura.router)
-# app.include_router(ubicacion.router)
 
+# Ruta base para probar que todo arranca
 @app.get("/")
 def root():
-    return {"message": "API ServiTech lista"}
+    return {"message": "La API está perfecta"}
